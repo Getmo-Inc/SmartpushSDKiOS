83da79e2189a4797967ce94d0a105f7f9dec0bcb
Carlos Corrêa <carlscorrea@gmail.com>
2019-01-31 14:11:37 -0200

    inbox ok

--- SmartpushSDKCore.framework/Versions/A/Headers/SmartpushSDKCore.h
+++ SmartpushSDKCore.framework/Versions/A/Headers/SmartpushSDKCore.h
@@ -28,6 +28,10 @@
 extern NSString * const SmartpushSDKDeviceAddedNotification;
 extern NSString * const SmartpushSDKUserInfoObtainedNotification;
 extern NSString * const SmartpushSDKBlockUserNotification;
+extern NSString * const SmartpushSDKLastNotificationsObtainedNotification;
+extern NSString * const SmartpushSDKUnreadNotificationsObtainedNotification;
+extern NSString * const SmartpushSDKExtraContentObtainedNotification;
+
 
 @property (strong, nonatomic) UIWindow *window;
 
@@ -46,9 +50,10 @@ extern NSString * const SmartpushSDKBlockUserNotification;
 
 - (id)getDevice;
 - (id)getUserInfo;
+- (id)getLastNotifications;
+- (id)getUnreadNotifications;
+- (id)getExtraContent;
 - (void)setValue:(NSString *)value forTag:(NSString *)key __deprecated_msg("use setString:foTag: instead.");
-
-- (NSString *)getStringForTag:(NSString *)key;
 
 - (void)setString:(NSString *)value forTag:(NSString *)key;
 - (void)setArray:(NSArray *)array forTag:(NSString *)key;
@@ -68,12 +73,18 @@ extern NSString * const SmartpushSDKBlockUserNotification;
 - (void)delBoolTag:(NSString *)key;
 - (void)delDateTag:(NSString *)key;
 
+- (void)markAsRead:(NSString *)pushid;
 - (void)nearestZoneWithLatitude:(double)latitude andLongitude:(double)longitude;
 - (void)blockUser:(BOOL)block;
 - (void)requestCurretUserInformation;
+- (void)requestLastNotifications;
+- (void)requestUnreadNotifications;
 - (void)sendHit:(SmartpushHitCore*)smartpushHit;
 - (void)sendHit:(SmartpushHitCore*)smartpushHit WithAction:(NSInteger) action;
 - (void)checkHitQueue;
+- (void)markPushAsRead:(NSString *)pushId;
+- (void)markAllAsRead;
+- (void)requestExtraContentFor:(NSString *)pushId;
 
 @property (weak, nonatomic) UIResponder<SmartpushSDKDelegateCore> * delegate;
 
